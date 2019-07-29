3430d9f045bf7f1a27e2589c1cb24ac6a4c7ba11
Aurélien Lebray <aurelien.lebray@epitech.eu>
2019-07-29 16:12:18 +0200

    Added cache thing to service worker

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
+var CACHE_NAME = 'ipt-cache'
+var urlsToCache = [
+    '/'
+]
+
 self.addEventListener('install', function(event) {
     console.log('install event catched');
+    event.waitUntil(
+	caches.open(CACHE_NAME)
+	    .then(function(cache) {
+		console.log('Opened cache');
+		return cache.addAll(urlsToCache);
+	    })
+    );
 });
